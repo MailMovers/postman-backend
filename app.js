ed0dafa9bdf1634d680a2f82eb21a35d4a2d5a8a
handyman-sodo <handymansodo@gmail.com>
2023-11-04 12:11:46 +0900

    초기세팅

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { errorHandler } = require("./src/utils/errorHandler");
+const { routes } = require("./src/routes");
+
+
+const createApp = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("combined"));
+
+  app.use(routes);
+  app.use(errorHandler);
+
+  return app;
+};
+
+module.exports = { createApp };
