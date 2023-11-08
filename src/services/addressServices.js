93ff60647ad285903396647d9582984aae360ce2
kimchiro <rlaehddhs12@gmail.com>
2023-11-08 17:16:09 +0900

    스테이징

--- src/services/addressServices.js
+++ src/services/addressServices.js
@@ -3,9 +3,12 @@ const {
   insertSendAddressDao,
   deleteSendAddressDao,
   deleteDeliveryAddressDao,
+<<<<<<< HEAD
   getSendListAddressDao,
   getDeliveryListAddressDao,
   getSendAddressDao,
+=======
+>>>>>>> d79eb7b (스테이징)
 } = require("../models/addressDao");
 
 const insertAddressService = async (
@@ -40,6 +43,7 @@ const insertSendAddressService = async (
   );
 };
 
+<<<<<<< HEAD
 const deleteSendAddressService = async (userId, sendAddressId) => {
   return await deleteSendAddressDao(userId, sendAddressId);
 };
@@ -59,12 +63,25 @@ const getDeliveryListAddressService = async (userId) => {
 const getSendAddressService = async (userId, sendAddressId) => {
   return await getSendAddressDao(userId, sendAddressId);
 };
+=======
+const deleteSendAddressService = async (userId, addressId) => {
+  return await deleteSendAddressDao(userId, addressId);
+};
+
+const deleteDeliveryAddressService = async (userId, addressId) => {
+  return await deleteDeliveryAddressDao(userId, addressId);
+};
+
+>>>>>>> d79eb7b (스테이징)
 module.exports = {
   insertAddressService,
   insertSendAddressService,
   deleteSendAddressService,
   deleteDeliveryAddressService,
+<<<<<<< HEAD
   getSendListAddressService,
   getDeliveryListAddressService,
   getSendAddressService,
+=======
+>>>>>>> d79eb7b (스테이징)
 };
