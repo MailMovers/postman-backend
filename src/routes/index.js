f7bae08d35c4627adaa294944c903171af99e647
kimchiro <rlaehddhs12@gmail.com>
2023-11-15 18:54:27 +0900

    Fix: Address test code file 복구

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const { addressRoute } = require("./addressRoutes");
+const { writingLetterRoute } = require("./writingLetterRoutes");
 
 const router = express.Router();
 
-
+router.use("/address", addressRoute);
+router.use("/letter", writingLetterRoute);
 
 module.exports = router;
