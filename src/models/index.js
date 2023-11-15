791c7e6855a0b3c5fd160fa65559ef0d8b64ebaf
kimchiro <rlaehddhs12@gmail.com>
2023-11-15 17:02:06 +0900

    wip

---
+++ src/models/index.js
@@ -0,0 +1,34 @@
+const {
+  insertDeliveryAddressDao,
+  insertSendAddressDao,
+  deleteSendAddressDao,
+  deleteDeliveryAddressDao,
+  getSendListAddressDao,
+  getDeliveryListAddressDao,
+  getSendAddressDao,
+  getDeliveryAddressDao,
+} = require("./addressDao");
+
+const {
+  letterDao,
+  photoDao,
+  countPhotoDao,
+  stampDao,
+  confirmLetterDao,
+} = require("./writingLetterDao");
+
+module.exports = {
+  insertDeliveryAddressDao,
+  insertSendAddressDao,
+  deleteSendAddressDao,
+  deleteDeliveryAddressDao,
+  getSendListAddressDao,
+  getDeliveryListAddressDao,
+  getSendAddressDao,
+  getDeliveryAddressDao,
+  letterDao,
+  photoDao,
+  countPhotoDao,
+  stampDao,
+  confirmLetterDao,
+};
