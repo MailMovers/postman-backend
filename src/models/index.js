02ef93884e6148dd9a0ef1f06fde31c39c49ccd3
ParkAsher <devasherpark@naver.com>
2023-11-22 23:00:27 +0900

    [feat] : 회원가입 쿼리 로직 구현

--- src/models/index.js
+++ src/models/index.js
@@ -1,34 +1,38 @@
 const {
-  insertDeliveryAddressDao,
-  insertSendAddressDao,
-  deleteSendAddressDao,
-  deleteDeliveryAddressDao,
-  getSendListAddressDao,
-  getDeliveryListAddressDao,
-  getSendAddressDao,
-  getDeliveryAddressDao,
-} = require("./addressDao");
+    insertDeliveryAddressDao,
+    insertSendAddressDao,
+    deleteSendAddressDao,
+    deleteDeliveryAddressDao,
+    getSendListAddressDao,
+    getDeliveryListAddressDao,
+    getSendAddressDao,
+    getDeliveryAddressDao,
+} = require('./addressDao');
 
 const {
-  letterDao,
-  photoDao,
-  countPhotoDao,
-  stampDao,
-  confirmLetterDao,
-} = require("./writingLetterDao");
+    letterDao,
+    photoDao,
+    countPhotoDao,
+    stampDao,
+    confirmLetterDao,
+} = require('./writingLetterDao');
+
+// user dao
+const UserDao = require('./userDao');
 
 module.exports = {
-  insertDeliveryAddressDao,
-  insertSendAddressDao,
-  deleteSendAddressDao,
-  deleteDeliveryAddressDao,
-  getSendListAddressDao,
-  getDeliveryListAddressDao,
-  getSendAddressDao,
-  getDeliveryAddressDao,
-  letterDao,
-  photoDao,
-  countPhotoDao,
-  stampDao,
-  confirmLetterDao,
+    insertDeliveryAddressDao,
+    insertSendAddressDao,
+    deleteSendAddressDao,
+    deleteDeliveryAddressDao,
+    getSendListAddressDao,
+    getDeliveryListAddressDao,
+    getSendAddressDao,
+    getDeliveryAddressDao,
+    letterDao,
+    photoDao,
+    countPhotoDao,
+    stampDao,
+    confirmLetterDao,
+    UserDao,
 };
