1976c620459fe15f11c98c90420d218662d28883
handyman-sodo <handymansodo@gmail.com>
2023-11-24 15:07:52 +0900

    wix

--- src/routes/writingLetterRoutes.js
+++ src/routes/writingLetterRoutes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const { writingLetterController } = require("../controllers");
 const {
-    letterContoller,
-    photoContoller,
-    confirmLetterContoller,
-    stampController,
-    saveOrUpdateAddressController
+  letterContoller,
+  photoContoller,
+  confirmLetterContoller,
+  stampController,
+  saveOrUpdateAddressController,
 } = writingLetterController;
 
 const writingLetterRoute = express.Router();
 
-writingLetterRoute.post('/write', letterContoller);
-writingLetterRoute.post('/photo', photoContoller);
-writingLetterRoute.post('/stamp', stampController)
-writingLetterRoute.post('/address', saveOrUpdateAddressController)
-writingLetterRoute.get('/confirm', confirmLetterContoller);
+writingLetterRoute.post("/write", letterContoller);
+writingLetterRoute.post("/photo", photoContoller);
+writingLetterRoute.post("/stamp", stampController);
+writingLetterRoute.post("/address", saveOrUpdateAddressController);
+writingLetterRoute.get("/confirm", confirmLetterContoller);
 
 module.exports = { writingLetterRoute };
