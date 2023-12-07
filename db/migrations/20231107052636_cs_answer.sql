2866e5a7e92c5541b2158d6dde54a5f8d744f73e
handyman-sodo <handymansodo@gmail.com>
2023-12-07 20:35:54 +0900

    [fix]고객센터 디비수정

--- db/migrations/20231107052636_cs_answer.sql
+++ db/migrations/20231107052636_cs_answer.sql
@@ -3,10 +3,11 @@ CREATE TABLE `cs_answer` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `content` varchar(500) NOT NULL,
   `user_id` int NOT NULL,
+  `customer_service_id` int NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp DEFAULT NULL
 );
 ALTER TABLE `cs_answer` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
+ALTER TABLE `cs_answer` ADD FOREIGN KEY (`customer_service_id`) REFERENCES `customer_service` (`id`);
 -- migrate:down
 DROP TABLE `cs_answer`
