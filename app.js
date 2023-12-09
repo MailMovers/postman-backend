431abdd759893571af810b2c2c8493b49f8b6a95
ParkAsher <devasherpark@naver.com>
2023-12-10 00:43:33 +0900

    [feat] : cors 설정 추가

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const passportConfig = require('./src/passport');
 const { errorHandler } = require('./src/utils/errorHandler');
 const router = require('./src/routes');
 
+// cors whitelist
+const whitelist = ['http://localhost:3000'];
+
 const createApp = () => {
     const app = express();
 
@@ -15,10 +18,15 @@ const createApp = () => {
         res.send('Hello World - Github Actions!');
     });
 
-    app.use(express.json());
-    app.use(cors());
-    app.use(morgan('dev'));
+    app.use(
+        cors({
+            origin: whitelist,
+            credentials: true,
+        })
+    );
     app.use(cookieParser());
+    app.use(express.json());
+    app.use(morgan('dev'));
 
     passportConfig(app);
     app.use(router);
