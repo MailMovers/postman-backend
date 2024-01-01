52ade49ad1e18ebcf658913b0283e2373f65c299
HANDY MAN <handymansodo@gmail.com>
2024-01-01 21:56:14 +0900

    [fix]user_deletion_reasonspk추가

--- db/schema.sql
+++ db/schema.sql
@@ -299,7 +299,9 @@ CREATE TABLE `stamps` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_deletion_reasons` (
-  `reason` text
+  `id` int NOT NULL AUTO_INCREMENT,
+  `reason` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
