ca3d3ff41448227c9920f48e7f831b266d3443ee
HANDY MAN <handymansodo@gmail.com>
2024-01-04 16:42:27 +0900

    [fix]orders, letters, customer_service ON DELETE SET NULL 추가

--- db/migrations/20231107052648_letters.sql
+++ db/migrations/20231107052648_letters.sql
@@ -4,14 +4,14 @@ CREATE TABLE `letters` (
   `page` int DEFAULT 1,
   `status` enum('save','delete') DEFAULT "save",
   `photo_count` int DEFAULT 0,
-  `user_id` int NOT NULL,
+  `user_id` int NULL,
   `writing_pad_id` int NOT NULL,
   `stamp_id` int NOT NULL,
   `send_address_id` int NOT NULL,
   `delivery_address_id` int NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-ALTER TABLE `letters` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `letters` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 ALTER TABLE `letters` ADD FOREIGN KEY (`writing_pad_id`) REFERENCES `writing_pads` (`id`);
 ALTER TABLE `letters` ADD FOREIGN KEY (`stamp_id`) REFERENCES `stamps` (`id`);
 ALTER TABLE `letters` ADD FOREIGN KEY (`send_address_id`) REFERENCES `send_address` (`id`);
