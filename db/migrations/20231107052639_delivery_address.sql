4517d982ed4d956f5832acfaffb4abc0c7ba178b
handyman <handymansodo@gmail.com>
2024-02-10 21:27:49 +0900

    [db]postCode 추가

--- db/migrations/20231107052639_delivery_address.sql
+++ db/migrations/20231107052639_delivery_address.sql
@@ -6,6 +6,7 @@ CREATE TABLE `delivery_address` (
   `delivery_address_detail` varchar(255) NOT NULL,
   `delivery_phone` varchar(255) NULL,
   `delivery_name` varchar(255) NOT NULL,
+  `post_code` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP
