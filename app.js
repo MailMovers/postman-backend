e0420a98a87d15a31985b3eb32fccadf9074b1d3
handyman-sodo <127203790+handyman-sodo@users.noreply.github.com>
2024-03-01 18:31:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const createApp = () => {
     const app = express();
 
     app.get('/', (req, res) => {
-        res.send('MailMovers - New Github Actions!');
+        res.send('MailMovers - New Github Actions2!');
     });
 
     app.use(
