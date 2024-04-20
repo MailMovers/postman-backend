8995c8886c8a598489fe2618b8d319d9d91c88b5
handyman-sodo <handymansodo@gmail.com>
2024-04-20 18:04:13 +0900

    [feat]종추적api 개발중

--- src/services/treckingService.js
+++ src/services/treckingService.js
@@ -3,11 +3,12 @@ const axios = require("axios");
 const url =
   "http://openapi.epost.go.kr/trace/retrieveLongitudinalCombinedService/retrieveLongitudinalCombinedService/getLongitudinalCombinedList";
 let queryParams = "?" + encodeURIComponent(process.env.DATAGOV_SECRET_KEY);
+
 const tracking = async (number) => {
   try {
     queryParams +=
       "&" + encodeURIComponent("rgist") + "=" + encodeURIComponent(number);
-    const response = await axios.get(url + queryParams);
+    const response = await axios.GET(url + queryParams);
     return response.data;
   } catch (error) {
     console.error(error);
